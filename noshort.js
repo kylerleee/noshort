6e91034c98ceb124f4c8dbaf3c991c2879c3ca2a
kyler <kylerlee68@gmail.com>
2026-02-14 14:23:45 +0800

    hide reels n reels tab, redirect reels

--- noshort.js
+++ noshort.js
@@ -2,6 +2,9 @@ const MINI_TAB = "ytd-guide-entry-renderer>a[title='Shorts']";
 const HOME_SHORTS = "ytd-rich-section-renderer";
 const UTUBER_SHORTS = "ytd-item-section-renderer ytd-reel-shelf-renderer";
 const UTUBER_SHORTS_SPECIFIC = "div ytm-shorts-lockup-view-model-v2";
+const IG_SHORTS_POST = "ul video";
+const IG_SHORTS = "article:has(video):not(:has(ul))";
+const IG_SHORTS_TAB = "a[href='/reels/']";
 
 function updateStyles(settings) {
   let styleEl = document.getElementById('shorts-hider-styles');
@@ -20,13 +23,22 @@ function updateStyles(settings) {
 
   else {
     styleEl.textContent = settings.HideUtubeShorts 
-    ? `${HOME_SHORTS} { display: none !important; }\\n`
-    + `${UTUBER_SHORTS} { display: none !important; }\\n`
+    ? `${HOME_SHORTS} { display: none !important; }\n`
+    + `${UTUBER_SHORTS} { display: none !important; }\n`
     + `${UTUBER_SHORTS_SPECIFIC} { display: none !important; }`
     : "";
 
     styleEl.textContent += settings.HideUtubeShortsTab 
-      ? `${MINI_TAB} { display: none !important; }`
+      ? `${MINI_TAB} { display: none !important; }\n`
+      : "";
+
+    styleEl.textContent += settings.HideIGShorts
+      ? `${IG_SHORTS_POST} { display: none !important; }\n`
+      + `${IG_SHORTS} { display: none !important; }\n`
+      : "";
+
+    styleEl.textContent += settings.HideIGShortsTab
+      ? `${IG_SHORTS_TAB} { display: none !important; }\n`
       : "";
   }
 
